d230c4684f915416ccc9a8a127fba7c56f00b92d
Andrew Pouliot <andrewpouliot@fb.com>
2015-01-12 14:28:19 -0800

    Ignore _site directory to avoid merge conflicts

--- _site/public/js/examples.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function($) {
-	console.log("examples page loaded");
-});
--- _site/public/js/global.js
+++
@@ -1,7 +0,0 @@
-var springSystem = new rebound.SpringSystem();
-
-// Utilities
-
-transition = function(progress, startValue, endValue) {
-	return startValue + (progress * (endValue - startValue));
-}
